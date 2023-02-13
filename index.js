5d8eb474a6ff3c904166d2985e763ff72c84e996
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-02-13 12:51:08 +0000

    created add books

---
+++ index.js
@@ -0,0 +1,56 @@
+var bookStore = []
+var bookItems = document.getElementById('bookItems');
+var bookTitle = document.getElementById('bookTitle');
+var bookAuthor = document.getElementById('bookAuthor');
+var addButton = document.getElementById('addButton');
+var removeBtn = document.getElementById('removeBtn')
+
+
+addButton.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    // add book to browser local storage
+    const addBook = (title, author) => {
+        const newBook = {
+            title: title,
+            author: author
+        }
+        bookStore.push(newBook)
+        localStorage.setItem('bookStore', JSON.stringify(bookStore))
+       
+    }
+    addBook(bookTitle.value, bookAuthor.value)
+
+    const removeBook = () => {
+        bookCollection = bookCollection.filter(book => book.title !== title)
+        localStorage.setItem('bookStore', JSON.stringify(bookStore))
+    }
+
+    // display
+    const displayBooks = () => {
+        for(let book = 0; book < bookStore.length; book += 1) {
+            const booksDiv = document.createElement('div')
+            const bookTitle = document.createElement('p')
+            bookTitle.innerHTML = book.title
+
+            const bookAuthor = document.createElement('p')
+            bookAuthor.innerHTML = book.author
+
+            const removeBook = document.createElement('button');
+            removeBook.id = 'removeBtn'
+            removeBook.innerHTML = 'Remove'
+
+            bookItems.appendChild(booksDiv)
+            booksDiv.appendChild(bookTitle)
+            booksDiv.appendChild(bookAuthor)
+            bookItems.appendChild(removeBook)
+        }
+
+    }
+
+    if(localStorage.getItem('bookStore')) {
+        bookStore = JSON.parse(localStorage.getItem('bookStore'))
+    }
+    displayBooks()
+     
+});
---
+++ index_.js
@@ -0,0 +1,36 @@
+// var bookStore = []
+// var addButton = document.getElementById("addButton")
+// var bookItems = document.getElementById("bookItems")
+// var bookTitle = document.getElementById("bookTitle").value;
+// var bookAuthor = document.getElementById("bookAuthor").value;
+// // console.log(bookTitle, bookAuthor)
+
+// addButton.addEventListener("click", () =>{
+//     // adding book to store
+//     const addBook = (bookTitle, bookAuthor) => {
+//         const newBook = {
+//             bookTitle: bookTitle,
+//             bookAuthor: bookAuthor
+//         };
+//         bookStore.push(newBook);
+//         localStorage.setItem("bookStore", JSON.stringify(bookStore))
+//     }
+//     addBook(bookTitle, bookAuthor)
+
+//     // display book detail to screen
+//     // const displayBooks = () => {
+//     //     for(let book = 0; book < bookStore.length; book += 1) {
+//     //         const title = document.createElement('p')
+//     //         title.innerHTML = book.bookTitle
+//     //         const author = document.createElement('span')
+//     //         author.innerHTML = book.bookAuthor;
+//     //         const removeBook = document.createElement('button');
+//     //         removeBook.innerHTML = 'Remove'
+//     //         bookItems.appendChild(title)
+//     //         bookItems.appendChild(author)
+//     //         bookItems.appendChild(removeBook)
+//     //     }
+//     // }
+//     //  displayBooks()
+// })
+   
