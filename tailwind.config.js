cd36b4eddf6c0cf5a02230ff8439f2e6a61190e5
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-02-15 16:25:23 +0000

    install and fixed linter error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 
-const daisyui = require('daisyui')
+const daisyui = require('daisyui');
 
 module.exports = {
   content: [],
@@ -8,12 +8,12 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: ['./dist/*.{html,js}'],
   theme: {
     extend: {},
   },
   plugins: [daisyui],
-}
+};
